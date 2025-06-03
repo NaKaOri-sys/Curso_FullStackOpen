81b7e54989074d6afe6ac6f7aec26c34c11f3dd8
Nahuel Nievas <nahuelnievas30@gmail.com>
2025-06-03 13:40:41 -0300

    feat: done exercise 1.4

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -4,15 +4,9 @@ const Content = (props) => {
     console.log(props)
     return (
         <>
-            <p>
-                <Part part={props.part1.name} exercise={props.part1.exercises} />
-            </p>
-            <p>
-                <Part part={props.part2.name} exercise={props.part2.exercises} />
-            </p>
-            <p>
-                <Part part={props.part3.name} exercise={props.part3.exercises} />
-            </p>
+            <Part part={props.part1.name} exercise={props.part1.exercises} />
+            <Part part={props.part2.name} exercise={props.part2.exercises} />
+            <Part part={props.part3.name} exercise={props.part3.exercises} />
         </>
     );
 }
