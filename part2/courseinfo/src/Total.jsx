07087b8f7bb27a138bfeef9760a5b63a5d3c7cc9
Nahuel Nievas <nahuelnievas30@gmail.com>
2025-06-25 20:36:28 -0300

    feat: done exercise 2.1

---
+++ part2/courseinfo/src/Total.jsx
@@ -0,0 +1,12 @@
+const Total = ({ parts }) => {
+    let totalExercises = 0;
+    parts.forEach(part => {
+        totalExercises += part.exercises; 
+    });
+    return (
+        <>
+            <p>Number of exercises {totalExercises}</p>
+        </>
+    );
+};
+export default Total;
