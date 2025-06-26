d912d0a43e4acdb08051a288d7c354538795c023
Nahuel Nievas <nahuelnievas30@gmail.com>
2025-06-25 21:03:33 -0300

    Update Total.jsx

--- part2/courseinfo/src/Total.jsx
+++ part2/courseinfo/src/Total.jsx
@@ -1,7 +1,7 @@
 const Total = ({ parts }) => {
     let totalExercises = parts.reduce((acum, part) => acum + part.exercises, 0);
     return (
-        <p>Number of exercises {totalExercises}</p>
+        <p><b>Number of exercises {totalExercises}</b></p>
     );
 };
 export default Total;
