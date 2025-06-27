3e94be0258c83c7e9adf7c5df3c73b5647ca1a9e
Nahuel Nievas <nahuelnievas30@gmail.com>
2025-06-27 09:06:53 -0300

    feat: done exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import Filter from './components/Filter';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        console.log(res);
+        setPersons(res.data);
+      });
+  };
+  useEffect(hook, []);
   const handleCreatePerson = (event) => {
     setNewName(event.target.value);
   };
   const handleCreateNumber = (event) => {
     setNewNumber(event.target.value);
   };
-  const handleFilterNumbers = (event) =>{
-    let val = event.target.value;  
+  const handleFilterNumbers = (event) => {
+    let val = event.target.value;
     const filteredNumbers = persons.filter((p) => (p.name.toLocaleLowerCase().includes(val)));
     setPersons(filteredNumbers);
   };
@@ -28,7 +33,7 @@ const App = () => {
     const objPerson = {
       name: newName,
       number: newNumber,
-      id: persons.length+1
+      id: persons.length + 1
     };
     if (persons.find((p) => (p.name == newName))) {
       alert(`${newName} is already added to phonebook.`);
@@ -44,7 +49,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter handleFilterNumbers={handleFilterNumbers}/>
+      <Filter handleFilterNumbers={handleFilterNumbers} />
       <h2>add a new</h2>
       <PersonForm person={handleEventsForm} />
       <h2>Numbers</h2>
